0140c323aad95d3d1b069d8eb8d77780762e3aa4
davidsph <daviddjy@163.com>
2012-12-05 14:37:08 +0800

    ok pro

---
+++ MyFlight2.0/MyFlight2.0/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MyFlight2.0
+//
+//  Created by Davidsph on 12/5/12.
+//  Copyright (c) 2012 LIAN YOU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ MyFlight2.0/MyFlight2.0/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  MyFlight2.0
+//
+//  Created by Davidsph on 12/5/12.
+//  Copyright (c) 2012 LIAN YOU. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.navigationController.navigationBarHidden  = YES;
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
