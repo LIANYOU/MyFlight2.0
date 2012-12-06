c3c57f84ae2aeab4d8e6f75abf864c1d63d6c754
davidsph <daviddjy@163.com>
2012-12-06 10:47:27 +0800

    new 1

---
+++ MyFlight2.0/MyFlight2.0/MyCenterController.h
@@ -0,0 +1,13 @@
+//
+//  MyCenterController.h
+//  MyFlight2.0
+//
+//  Created by Davidsph on 12/6/12.
+//  Copyright (c) 2012 LIAN YOU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyCenterController : UIViewController
+
+@end
